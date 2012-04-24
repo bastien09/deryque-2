7d07e52231a485965cd3b49c493bf4b198c7725a
anthony.brao@gmail.com <anthony.brao@gmail.com@d9a5d21d-1882-c1c2-608c-296918f10cd2>
2012-04-24 16:58:23 +0000

    Ecriture de HL7File.php

--- trunk/FileTypes/HL7File.php
+++ trunk/FileTypes/HL7File.php
@@ -7,11 +7,27 @@ class HL7File implements FileType {
 		return FALSE;
 	}
 	
-	public static function recupDonneesImportables($fichier) {
-		
-		//TODO Anthony
-		
+	private static function table($digit, $value) {
+		$table = array();
+		$table = preg_split ( "/[\s,]+/" , $digit );
+		$res = array();
+  		for($i = 0; $i < $table.length; $i++ ) {
+   			$res[$i*$value] = $table[$i];
+  		}
+		return $res;
 	}
-
+	
+	public static function recupDonneesImportables($fichier) {
+		$dom = new DOMDocument;
+		$dom->loadXML($fichier);
+		$sequence = $dom->getElementsByTagName('sequenceSet');
+		$value = $sequence->getElementsByTagName('increment')->getAttributes('value');
+		$digits = $sequence->getElementsByTagName('digits')
+		$tableau = array();
+		$i = 0;
+		foreach ($digits as $digit) {
+			$tableau[i] = table($digit,$value);
+		}
+	}
 }
 ?>
