0892f863b1233e238613f55a0501d196a37e020f
damienvdb06@gmail.com <damienvdb06@gmail.com@d9a5d21d-1882-c1c2-608c-296918f10cd2>
2012-04-25 15:22:17 +0000

    Toggle bugués, Taille de l'iframe magnifique

--- trunk/Display/GrapheTempsReel/DGrapheTempsReel.php
+++ trunk/Display/GrapheTempsReel/DGrapheTempsReel.php
@@ -5,9 +5,9 @@ class DGrapheTempsReel extends DAbstract {
 	public function show() {
 		if ($this -> gererVide())
 			return;
-
-		CHead::addJs('jquery-1.6.2.min');
-		CHead::addJs('jquery.flot.min');
+		
+		CHead::addJs('jquery-ui-1.8.19.custom.min');
+		//CHead::addJs('jquery.flot.min');
 		//CHead::addJs('Plot_js/jquery_realtimelinechart');
 		CHead::addJs('highstock');
 		CHead::addJs('exporting');
@@ -43,16 +43,49 @@ class DGrapheTempsReel extends DAbstract {
 		$addCharts .= "</script>";
 
 		echo <<<END
-		<div id='holder' style="margin:20px;"></div>
-		<div id='droite'>
-		<button onClick="setAction('flags');"> Flags </button>
-		<button onClick="setAction('yline');"> Line on YAxis </button>
-		<button onClick="setAction('xline');"> Line on XAxis  </button>
-		<button onClick="run = false;"> Stop  </button>
-		<button onClick="run = true;requestData(lastCall);"> Go  </button>
-		<button onClick="Dezoom();"> Zoom out </button>
-		<input type="text" id="nb" /><button onClick="rmChart($('#nb').val());"> RmChart </button>
+		<script>
+		jQuery(document).ready(function(){
+			$('#head').click(function() {
+				$(this).next().toggle('slow');
+				return false;
+			}).next().hide();
+		});
+			$(function() {
+				$( "#accordion" ).accordion({
+					collapsible: true
+				});
+			});
+		</script>
+		<div id="englober" style="height:650px;">
+		<div id='holder' style="margin:20px;float:left;"></div>
+			<div id='accordion'>
+				<div class="well">
+					<h3 id="head">Controles</h3>
+					<div>				
+						<button onClick="setAction('flags');"> Flags </button>
+						<button onClick="setAction('yline');"> Line on YAxis </button>
+						<button onClick="setAction('xline');"> Line on XAxis  </button>
+						<button onClick="run = false;"> Stop  </button>
+						<button onClick="run = true;requestData(lastCall);"> Go  </button>
+						<button onClick="Dezoom();"> Zoom out </button>
+						<input type="text" id="nb" /><button onClick="rmChart($('#nb').val());"> RmChart </button>
+					</div>
+				</div>
+				<div class="well">
+					<h3 id="head">Informations</h3>
+					<div>Texte1</div>
+				</div>
+				<div class="well">
+					<h3 id="head">Marqueurs</h3>
+					<div>Texte1</div>
+				</div>
+				<div class="well">
+					<h3 id="head">Detection de pics</h3>
+					<div>Texte1</div>
+				</div>
+			</div>
 		</div>
+
 END;
 
 		echo $addCharts;
