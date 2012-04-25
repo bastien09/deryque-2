2b438ef25104bf5e92eefa7b71e5c61219e105aa
Bastien Auda <bastien09@users.noreply.github.com>
2012-04-25 15:21:58 +0000

    Debuggage du code d'Anthony.

--- FileTypes/HL7File.php
+++ FileTypes/HL7File.php
@@ -7,28 +7,31 @@ class HL7File implements FileType {
 		return TRUE;
 	}
 	
-	private static function table($digit, $value) {
+	private static function table($digit) {
 		$table = array();
-		$table = preg_split ( "/[\s]+/" , $digit );
-		$res = array();
-  		for($i = 0; $i < $table.length; $i++ ) {
-   			$res[$i*$value] = $table[$i];
-  		}
-		return $res;
+		$table = preg_split ( "/[\s]+/" , $digit, NULL, PREG_SPLIT_NO_EMPTY );
+		return $table;
 	}
 	
 	public static function recupDonneesImportables($fichier) {
-		$dom = new DOMDocument();
-		$dom->loadXML($fichier);
-		$sequence = $dom->getElementsByTagName('sequenceSet');
-		$value = $sequence->getElementsByTagName('increment')->getAttributes('value');
+		$dom = new DOMDocument("1.0","utf-8");
+		$dom->load($fichier);
+		$sequence = $dom->getElementsByTagName('sequenceSet')->item(0);
+		$increment = $sequence->getElementsByTagName('increment')->item(0)->getAttribute('value');
 		$digits = $sequence->getElementsByTagName('digits');
+		/** tableaux[0] = timestamp, tableaux[1+] = valeurs */
 		$tableaux = array();
+		$i = 1;
+		/** on extrait les séquences */
 		foreach ($digits as $digit) {
-			$tableaux[] = table($digit,$value);
+			$tableaux[$i] = self::table($digit->nodeValue);
+			$i++;
 		}
-		
-		
+		/* on remplit le timestamp */
+		for($j = 0; $j < count($tableaux[1]); $j++) {
+			$tableaux[0][] = $j*$increment;
+		}
+				
 		echo <<<END
 		<table class="bordered-table">
 			<tr>
