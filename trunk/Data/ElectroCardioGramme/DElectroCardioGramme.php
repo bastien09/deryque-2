8c7a440007a8d3c1fef4d4b5b1a1e29e4feb4699
bastien.auda <bastien.auda@d9a5d21d-1882-c1c2-608c-296918f10cd2>
2012-04-25 13:02:53 +0000

    Mise à jour du fichier Data.

--- trunk/Data/ElectroCardioGramme/DElectroCardioGramme.php
+++ trunk/Data/ElectroCardioGramme/DElectroCardioGramme.php
@@ -1,47 +1,14 @@
 <?php
 class DElectroCardioGramme
 {
-	const nom = 'Rythme cardiaque';
+	const nom = 'Electrocardiogramme';
 	
 	const n_timestamp = 'Date';
 	public $timestamp;
 
-	const n_MDC_ECG_LEAD_I = 'MDC_ECG_LEAD_I';
-	public $MDC_ECG_LEAD_I;
+	const n_voltage = 'Voltage';
+	public $voltage;
 	
-	const n_MDC_ECG_LEAD_II = 'MDC_ECG_LEAD_II';
-	public $MDC_ECG_LEAD_II;
-	
-	const n_MDC_ECG_LEAD_III = 'MDC_ECG_LEAD_III';
-	public $MDC_ECG_LEAD_III;
-	
-	const n_MDC_ECG_LEAD_V1 = 'MDC_ECG_LEAD_V1';
-	public $MDC_ECG_LEAD_V1;
-	
-	const n_MDC_ECG_LEAD_V2 = 'MDC_ECG_LEAD_V2';
-	public $MDC_ECG_LEAD_V2;
-	
-	const n_MDC_ECG_LEAD_V3 = 'MDC_ECG_LEAD_V3';
-	public $MDC_ECG_LEAD_V3;
-	
-	const n_MDC_ECG_LEAD_V4 = 'MDC_ECG_LEAD_V4';
-	public $MDC_ECG_LEAD_V4;
-	
-	const n_MDC_ECG_LEAD_V5 = 'MDC_ECG_LEAD_V5';
-	public $MDC_ECG_LEAD_V5;
-	
-	const n_MDC_ECG_LEAD_V6 = 'MDC_ECG_LEAD_V6';
-	public $MDC_ECG_LEAD_V6;
-	
-	const n_MDC_ECG_LEAD_AVR = 'MDC_ECG_LEAD_AVR';
-	public $MDC_ECG_LEAD_AVR;	
-	
-	const n_MDC_ECG_LEAD_AVL = 'MDC_ECG_LEAD_AVL';
-	public $MDC_ECG_LEAD_AVL;
-	
-	const n_MDC_ECG_LEAD_AVF = 'MDC_ECG_LEAD_AVF';
-	public $MDC_ECG_LEAD_AVF;
-	
-	const display_prefs = 'Graphe Tableau';
+	const display_prefs = 'Graphe RT';
 }
 ?>
--- trunk/FileTypes/HL7File.php
+++ trunk/FileTypes/HL7File.php
@@ -4,12 +4,12 @@ class HL7File implements FileType {
 
 
 	public static function isOfThisDataType($fichier,$extension) {
-		return FALSE;
+		return TRUE;
 	}
 	
 	private static function table($digit, $value) {
 		$table = array();
-		$table = preg_split ( "/[\s,]+/" , $digit );
+		$table = preg_split ( "/[\s]+/" , $digit );
 		$res = array();
   		for($i = 0; $i < $table.length; $i++ ) {
    			$res[$i*$value] = $table[$i];
@@ -18,16 +18,33 @@ class HL7File implements FileType {
 	}
 	
 	public static function recupDonneesImportables($fichier) {
-		$dom = new DOMDocument;
+		$dom = new DOMDocument();
 		$dom->loadXML($fichier);
 		$sequence = $dom->getElementsByTagName('sequenceSet');
 		$value = $sequence->getElementsByTagName('increment')->getAttributes('value');
-		$digits = $sequence->getElementsByTagName('digits')
-		$tableau = array();
-		$i = 0;
+		$digits = $sequence->getElementsByTagName('digits');
+		$tableaux = array();
 		foreach ($digits as $digit) {
-			$tableau[i] = table($digit,$value);
+			$tableaux[] = table($digit,$value);
 		}
+		
+		
+		echo <<<END
+		<table class="bordered-table">
+			<tr>
+				<th><input type="checkbox" value="option1" name="optionsCheckboxes"/></th>
+				<th>Sequences</th>
+			</tr>
+END;
+
+
+		//TODO ajouter données
+		
+		echo <<<END
+		</table>
+END;
+		
+		
 	}
 }
 ?>
