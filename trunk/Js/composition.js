97830fd91dd58f9c945467d0b0ea72ecbe5374dc
bastien.auda <bastien.auda@d9a5d21d-1882-c1c2-608c-296918f10cd2>
2012-05-27 16:59:17 +0000

    Ajout de fonctions pour supprimer les compositions et sélections. Tentative de faire fonctionner le js des compositions (Highcharts error #13)

--- trunk/Js/composition.js
+++ trunk/Js/composition.js
@@ -1,10 +1,18 @@
 var compositions = [];
 var dataCharts = [];
 
-var addComposition = function(cname) {
-	compositions[cname] = [];
-}
+var graphs = [];
 
+var addComposition = function(cname) {
+
+	compo = new Object();
+
+	compo.name = cname;
+
+	compo.selections = [];
+
+	compositions.push(compo);
+}
 /**
  * Ajoute une sélection à la composition donnée.
  * @param {Object} cname Le nom de la composition
@@ -12,7 +20,7 @@ var addComposition = function(cname) {
  * @param {Object} debut
  * @param {Object} fin
  */
-var addSelectionToComposition = function(cname,graphName,debut,fin) {
+var addSelectionToComposition = function(cname, graphName, debut, fin) {
 	var selection = new Object();
 	var section = new Object();
 	section.min = debut;
@@ -20,85 +28,96 @@ var addSelectionToComposition = function(cname,graphName,debut,fin) {
 	selection.graph = graphName;
 	selection.section = section;
 
-	compositions[cname].push(selection);
+	for(var i = 0; i < compositions.length; i++) {
+		if(compositions[i].name == cname) {
+			compositions[i].selections.push(selection);
+		}
+	}
 }
-
-var showBigChart = function(divName, composition) {
-	$('#'+ divName).append('<div id="chartBloc"><a class="close" href="#" onClick="rmChart(\''+ divName +'\', '+i+')">x</a><div id="'+divName+''+i+'" style="margin:20px;"></div></div>');
-	where.push(
-			new Highcharts.StockChart({
-				chart : {
-					renderTo : divName+''+i+'-'+j,
-					height:300,
-					width:600
-				},
-				title : {
-					text : title
-				},
-				rangeSelector : {
-					buttons: [{
-						count: 1,
-						type: 'second',
-						text: '1s'
-					},
-					{
-						count: 10,
-						type: 'second',
-						text: '10s'
-					},
-					{
-						count: 30,
-						type: 'second',
-						text: '30s'
-					}, {
-						count: 1,
-						type: 'minute',
-						text: '1min'
-					}, {
-						type: 'all',
-						text: 'All'
-					}],
-					inputEnabled: false,
-					selected: 0
-				},
-				series : [{
-					name : title,
-					data : computeData(i, composition)
-				}],
-				exporting : {
-					enabled : false
-				}
-			}));
+/**
+ * Affiche un miniChart
+ * @param {Object} where Dans quel tableau ajouter le graphe
+ * @param {Object} divName Dans quel container
+ * @param {Object} i Utilis� pour retrouver l'indice du tableau du pics, mais aussi sa ligne dans la grille de divs
+ * @param {Object} j Sa colonne dans la grille de divs
+ * @param {Object} tab
+ */
+var showBigChart = function(where, divName, i, j, section, title) {
+	$('#' + divName).append('<div id="chartBloc"><a class="close" href="#" onClick="rmChart(\'' + divName + '\', ' + i + ')">x</a><div id="' + divName + '' + i + '" style="margin:20px;"></div></div>');
+	where.push(new Highcharts.StockChart({
+		chart : {
+			renderTo : divName + '' + i + '-' + j,
+			height : 300,
+			width : 600
+		},
+		title : {
+			text : title
+		},
+		rangeSelector : {
+			buttons : [{
+				count : 1,
+				type : 'second',
+				text : '1s'
+			}, {
+				count : 10,
+				type : 'second',
+				text : '10s'
+			}, {
+				count : 30,
+				type : 'second',
+				text : '30s'
+			}, {
+				count : 1,
+				type : 'minute',
+				text : '1min'
+			}, {
+				type : 'all',
+				text : 'All'
+			}],
+			inputEnabled : false,
+			selected : 0
+		},
+		series : [{
+			name : title,
+			data : computeData(i, section)
+		}],
+		exporting : {
+			enabled : false
+		}
+	}));
 
 };
 
-var computeData = function(i, composition) {
-	var indice = 0;
+var computeData = function(i, selection, title) {
+	section = selection.section;
+	console.log(section);
 	var tmp = [];
-	$.each(composition, function (i, compo) {
-		for (var x = 0; x < dataCharts.length; x++) {
-			if (compo.graph == dataCharts[x].name) {
-				indice = x; 
-				break;
-			}	
+	var indice = 0;
+	for (var y = 0; y < dataCharts.length; y++) {
+		if (dataCharts[y].name == title) {
+			console.log('Gg');
+			indice = y;
+			break;
 		}
-		for(var x = 0; x < dataCharts[indice].length; x++) {
-			var leX = dataCharts[indice][x].x*1000;
-			if (leX >= compo.section.max)
-				break;
-			if (leX >= compo.section.min) {
-				tmp.push([leX, dataCharts[indice][x].y]);
-			}
+	}
+	for(var x = 0; x < dataCharts[indice].length; x++) {
+		var leX = dataCharts[indice][x].x*1000;
+		if (leX >= section.max)
+			return tmp;
+		if (leX >= section.min) {
+			tmp.push([leX, dataCharts[indice][x].y]);
 		}
-	});
+	}
 	return tmp;
 };
 
 var initCompositions = function() {
 	//TODO utiliser compositions ( modifie le remplissage du tableau composition comme tu veux)
 	console.log(compositions);
-	$.each(compositions, function (i, compo) {
+	$.each(compositions, function(i, compo) {
 		console.log("Une compo");
-		showBigChart('composition', compo);
+		$.each(compo.selections, function(j, selection) {
+			showBigChart(graphs, 'composition', i, j, selection, compo.name);
+		});
 	});
 }
